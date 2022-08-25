2ad5d42ff74638f64546930110690049cd6bd489
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-25 21:41:44 +0000

    name page created

---
+++ pages/pokemon/name/[name].tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../../components/layout";
+
+const PokemonByNamePage = () => {
+  return (
+    <Layout title="Pokemon Name">
+      <h1>Pokemon Name</h1>
+    </Layout>
+  );
+};
+
+export default PokemonByNamePage;
