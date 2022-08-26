566cbd20d9edf87b06bf90c4491c4ec671376efb
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-26 03:20:37 +0000

    og image

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -7,17 +7,26 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout: FC<Props> = (props) => {
   return (
     <>
       <Head>
         <title>{props.title || "Pokemon App"}</title>
         <meta name="author" content="Isai Reyes" />
-        <meta name="description" content="Info about ${name of the pokemon}" />
+        <meta name="description" content={`Info about ${props.title}`} />
+        <meta name="keywords" content={`${props.title}, pokemon, pokedex`} />
+
         <meta
-          name="keywords"
-          content="${name of the pokemon}, pokemon, pokedex"
+          property="og:title"
+          content={`Information about ${props.title}`}
         />
+        <meta
+          property="og:description"
+          content={`Content about ${props.title}`}
+        />
+        <meta property="og:image" content={`${origin}/images/banner.png`} />
       </Head>
       <Navbar />
       <main
